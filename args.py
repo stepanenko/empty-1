efbce82b24fb347b95395c055f8629bacafb7858
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2021-03-26 23:01:41 +0200

    Add files via upload

---
+++ args.py
@@ -0,0 +1,21 @@
+
+def multiply(*values):  # asterisk will convert the 'values' to tuple
+    total = 1
+    for value in values:
+        total *= value
+    return total
+
+
+print(multiply(1, 2, 3, 4))  # 24
+
+
+def get_user(**user):
+    print(user)
+    print(user["id"])
+    print(user["name"])
+
+
+get_user(id=1, name="John")
+# {'id': 1, 'name': 'Jon'} - dictionary, (object in JS)
+# 1
+# John
